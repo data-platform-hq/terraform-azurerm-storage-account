139a7c5d209afb4f5a81a6bc00e278df2dae1634
omartushevskyi <oleh_martushevskyi@epam.com>
2023-01-27 12:30:03 -0500

    feat: updated monitoring configuration

--- variables.tf
+++ variables.tf
@@ -135,18 +135,6 @@ variable "log_category_list" {
   ]
 }
 
-variable "log_retention_days" {
-  default     = 7
-  description = "Retention log policy days"
-  type        = number
-}
-
-variable "metric_retention_days" {
-  default     = 7
-  description = "Metric policy days"
-  type        = number
-}
-
 variable "destination_type" {
   type        = string
   description = "Log analytics destination type"
